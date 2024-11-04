acd366bdbb2bbdc32bbfd7b4b92bad76b7e8d50b
Edgar Costa <edgarc@mit.edu>
2024-11-04 15:05:16 -0500

    FldRe and FldCom

--- NumericalSolve.m
+++ NumericalSolve.m
@@ -23,26 +23,11 @@ end function;
 
 declare verbose NewtonRaphson, 1;
 
-
-/*
-
-> V := VectorSpace(ComplexField(), 1);
-> f := map<V->V|x:->Vector([x[1]^2 + x[1] - 1])>;
-> AttachSpec("Torsion.spec"); NewtonRaphson(f, Vector([ComplexField() | -1.618]));
-(-1.61803398874989484820458683437)
-*/
-
-intrinsic NewtonRaphson(f::Map[ModTupFld[FldCom], ModTupFld[FldCom]], x0::ModTupFldElt[FldCom] : Derivative:=false, fx0:=false, prec:=false) -> ModTupFld[FldCom]
-{
-  Newton-Raphson method to solve f(x) = 0, where f is function between a n-dimensional complex vector space, starting at x = x0.
-  The Derivative may be given as a map df : C^n -> C^(n * n), otherwise, numerical differentiation will be used via forward Euler method.
-  fx0 is optional value for f(x0).
-  prec is the desired accuracy.
-}
-  require Domain(f) eq Codomain(f) : "We expect the domain and codomain of f to identical";
-  require Parent(x0) eq Domain(f) : "We expect x0 to be in the domain of f";
-  // This implements Newton-Raphson for a function C^n -> C^n.
-  // This even works when the derivative is not available; numerical differentiation will be used.
+// This implements Newton-Raphson for a function C^n -> C^n.
+// This even works when the derivative is not available; numerical differentiation will be used.
+function NewtonRaphsonFn(f, x0 : Derivative:=false, fx0:=false, prec:=false)
+  assert Domain(f) eq Codomain(f);// "We expect the domain and codomain of f to identical";
+  assert Parent(x0) eq Domain(f); // "We expect x0 to be in the domain of f";
   n := 0;
   if prec cmpeq false then
     prec := Precision(BaseRing(x0));
@@ -72,6 +57,41 @@ intrinsic NewtonRaphson(f::Map[ModTupFld[FldCom], ModTupFld[FldCom]], x0::ModTup
     vprint NewtonRaphson: "Newton-Raphson step", n, ": ", dif;
   end while;
   return xn;
+end function;
+
+
+/*
+
+SetDefaultRealFieldPrecision(100);
+V := VectorSpace(ComplexField(), 1);
+f := map<V->V|x:->Vector([x[1]^2 + x[1] - 1])>;
+NewtonRaphson(f, Vector([ComplexField() | -1.618]));
+
+V := VectorSpace(RealField(), 1);
+f := map<V->V|x:->Vector([x[1]^2 + x[1] - 1])>;
+NewtonRaphson(f, Vector([-1.618]));
+(-1.618033988749894848204586834365638117720309179805762862135448622705260462818902449707207204189391137)
+*/
+
+intrinsic NewtonRaphson(f::Map[ModTupFld[FldCom], ModTupFld[FldCom]], x0::ModTupFldElt[FldCom] : Derivative:=false, fx0:=false, prec:=false) -> ModTupFld[FldCom]
+{
+  Newton-Raphson method to solve f(x) = 0, where f is function between a n-dimensional complex vector space, starting at x = x0.
+  The Derivative may be given as a map df : C^n -> C^(n * n), otherwise, numerical differentiation will be used via forward Euler method.
+  fx0 is optional value for f(x0).
+  prec is the desired accuracy.
+}
+  require Domain(f) eq Codomain(f) : "We expect the domain and codomain of f to identical";
+  x0 := Domain(f)!x0;
+  require Parent(x0) eq Domain(f) : "We expect x0 to be in the domain of f";
+  return NewtonRaphsonFn(f, x0 : Derivative:=Derivative, fx0:=fx0, prec:=prec);
+end intrinsic;
+
+intrinsic NewtonRaphson(f::Map[ModTupFld[FldRe], ModTupFld[FldRe]], x0::ModTupFldElt[FldRe] : Derivative:=false, fx0:=false, prec:=false) -> ModTupFld[FldRe]
+{ " } //"
+  require Domain(f) eq Codomain(f) : "We expect the domain and codomain of f to identical";
+  x0 := Domain(f)!x0;
+  require Parent(x0) eq Domain(f) : "We expect x0 to be in the domain of f";
+  return NewtonRaphsonFn(f, x0 : Derivative:=Derivative, fx0:=fx0, prec:=prec);
 end intrinsic;
 
 
@@ -83,6 +103,20 @@ end intrinsic;
 
 
 intrinsic NewtonRaphson(f::Map[ModTupFld[FldCom], ModTupFld[FldCom]], x0::ModMatFldElt[FldCom] : Derivative:=false, fx0:=false, prec:=false) -> ModMatFldElt[FldCom]
+{ " } //"
+  x := NewtonRaphson(f, Vector(x0) : Derivative:=Derivative, fx0:=fx0, prec:=prec);
+  return Matrix([x]);
+end intrinsic;
+
+
+intrinsic NewtonRaphson(f::Map[ModTupFld[FldRe], ModTupFld[FldRe]], x0::SeqEnum[FldReElt] : Derivative:=false, fx0:=false, prec:=false) -> SeqEnum[FldReElt]
+{ " } //"
+  x := NewtonRaphson(f, Vector(x0) : Derivative:=Derivative, fx0:=fx0, prec:=prec);
+  return Eltseq(x);
+end intrinsic;
+
+
+intrinsic NewtonRaphson(f::Map[ModTupFld[FldRe], ModTupFld[FldRe]], x0::ModMatFldElt[FldRe] : Derivative:=false, fx0:=false, prec:=false) -> ModMatFldElt[FldRe]
 { " } //"
   x := NewtonRaphson(f, Vector(x0) : Derivative:=Derivative, fx0:=fx0, prec:=prec);
   return Matrix([x]);
